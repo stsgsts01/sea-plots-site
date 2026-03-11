1f50ad0733241e873cf6dd8834f3d9170bd2b6c7
stsgsts01 <stsgsts01@gmail.com>
2026-03-12 00:31:27 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -83,7 +83,7 @@ window.initSeaPlotsMap = function () {
   const group = L.featureGroup([p320, p220, p140, p67, p9]);
 
 map.fitBounds(group.getBounds(), {
-  padding: [30, 30]
+  padding: [45, 45]
 });
 
 map.on("zoomend", updateLabels);
