9d3350b5b25b9b9e71d19f71ea06613313e6c165
stsgsts01 <stsgsts01@gmail.com>
2026-03-11 15:33:27 +0300

    Update plots-data.js

--- js/plots-data.js
+++ js/plots-data.js
@@ -1,1 +1,97 @@
+window.PLOTS_DATA = {
+  plot320: {
+    id: "plot320",
+    page: "plot-320.html",
 
+    title: "Участок 320 соток",
+    shortTitle: "320 соток",
+
+    status: "available", // available | reserve | sold | hidden
+    statusText: "В продаже",
+
+    price: "100 000 000 ₽",
+    priceNote: "Актуальная стоимость участка. Подробные материалы предоставляются по запросу.",
+
+    cadastralNumber: "90:11:240801:1990",
+
+    area: "320 соток",
+    seaLine: "Первая линия",
+    distanceToSea: "Около 100 метров",
+    distanceToBeach: "Непосредственный выход на пляж",
+
+    location: "Крым, район Евпатории, побережье Чёрного моря",
+
+    suitableFor: "Гостиницы, апарт-отеля, базы отдыха, дома отдыха, курортного комплекса",
+    access: "Подъезд по существующей дороге",
+
+    heroEyebrow: "инвестиционный земельный участок у моря",
+    heroTitle: "320 соток",
+    heroLead: "Крупный участок у моря в районе Евпатории. Первая линия, около 100 метров до пляжа, высокий визуальный контакт с побережьем и формат, подходящий для гостиничного, туристического или курортного проекта.",
+
+    pills: [
+      "Первая линия",
+      "~100 м до моря",
+      "Непосредственный выход на пляж",
+      "Кадастровый № 90:11:240801:1990"
+    ],
+
+    miniStats: [
+      { label: "Площадь", value: "320 соток" },
+      { label: "Удалённость от моря", value: "~100 м" },
+      { label: "Формат", value: "Инвесторский лот" },
+      { label: "Назначение", value: "Туристический проект" }
+    ],
+
+    gallery: [
+      {
+        src: "img/plots/320/1990-6.jpg",
+        alt: "Участок 320 соток — общий вид с дрона",
+        captionLeft: "Участок 320 соток • общий вид",
+        captionRight: "Открыть фото"
+      },
+      {
+        src: "img/plots/320/1990-3.jpg",
+        alt: "Участок 320 соток с выделенными границами",
+        captionLeft: "Фото 1",
+        captionRight: "Границы участка"
+      },
+      {
+        src: "img/plots/320/1990-4.jpg",
+        alt: "Участок 320 соток с видом вдоль моря",
+        captionLeft: "Фото 2",
+        captionRight: "Вид вдоль берега"
+      },
+      {
+        src: "img/plots/320/1990-5.jpg",
+        alt: "Участок 320 соток, вид с побережья",
+        captionLeft: "Фото 3",
+        captionRight: "Контур и побережье"
+      },
+      {
+        src: "img/plots/320/1990-7.jpg",
+        alt: "Участок 320 соток, схема сверху",
+        captionLeft: "Фото 4",
+        captionRight: "Схема сверху"
+      }
+    ],
+
+    map: {
+      lat: 45.230,
+      lng: 33.040,
+      zoom: 14,
+      popup: "Участок 320 соток<br>Первая линия моря"
+    },
+
+    schemeImage: {
+      src: "img/plots/320/1990-1.jpg",
+      alt: "Кадастровая схема участка 320 соток"
+    },
+
+    docs: {
+      cadastralOpen: "docs/plot-320/kadastr/index.html",
+      cadastralPdf: "docs/plot-320/kadastr/vypiska.pdf",
+      presentationOpen: "docs/plot-320/presentation/index.html",
+      videoOpen: "video/plot-320/index.html"
+    }
+  }
+};
