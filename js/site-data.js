1ce4f827766dc36c10d309550bf3b705a5d6b8a6
stsgsts01 <stsgsts01@gmail.com>
2026-03-13 19:53:47 +0300

    Update site-data.js

--- js/site-data.js
+++ js/site-data.js
@@ -5,13 +5,13 @@ window.SITE_DATA = {
   contacts: {
 
     phone1: {
-      display: "+7 (978) 123-45-67",
-      link: "+79781234567"
+      display: "+7 (926) 086-31-41",
+      link: "+79260863141"
     },
 
     phone2: {
-      display: "+7 (978) 987-65-43",
-      link: "+79789876543"
+      display: "+7 (978) 511-63-98",
+      link: "+79785116398"
     },
 
     emailUser: "info",
