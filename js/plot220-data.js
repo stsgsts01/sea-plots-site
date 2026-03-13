5eef2a79a9944601500c2459aa648a60e97df248
stsgsts01 <stsgsts01@gmail.com>
2026-03-13 15:05:55 +0300

    Add files via upload

---
+++ js/plot220-data.js
@@ -0,0 +1,30 @@
+window.PLOT_DATA = {
+    key: "plot220",
+    slug: "plot-220.html",
+    title: "220 соток",
+    heroTitle: "220 соток",
+    heroEyebrow: "инвестиционный земельный участок у моря",
+    heroLead: "Крупный участок первой линии у моря в районе Евпатории. Подходит для гостиничного, апартаментного или туристического проекта.",
+    price: "300 000 000 ₽",
+    priceNote: "Техническая цена-заглушка. Позже можно заменить на актуальную стоимость без изменения HTML-страницы.",
+    area: "220 соток",
+    cadastralNumber: "90:11:150401:246",
+    seaLine: "Первая линия",
+    distanceToSea: "Около 100 метров",
+    distanceToBeach: "Прямой выход на пляж",
+    location: "Крым, район Евпатории, Витино, побережье Чёрного моря",
+    suitableFor: "Гостиница, апарт-отель, база отдыха, туристический проект",
+    access: "Подъезд по существующей дороге",
+    status: "active",
+    statusText: "В продаже",
+    pills: [
+      "Первая линия",
+      "~100 м до моря",
+      "Инвесторский лот",
+      "Курортный формат"
+    ],
+    docs: {
+      cadastralOpen: "docs/plot-220/kadastr/index.html",
+      presentationOpen: "docs/plot-220/presentation/index.html",
+      videoFile: "video/220/drone220.mp4"
+    }
