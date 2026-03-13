355e1c5060ea941ff5fb2e4357e0c590be94c2c3
stsgsts01 <stsgsts01@gmail.com>
2026-03-14 00:25:31 +0300

    Add files via upload

---
+++ js/index-data.js
@@ -0,0 +1,42 @@
+window.INDEX_DATA = {
+
+plots: [
+
+{
+title: "320 соток",
+link: "plot-320.html",
+image: "img/plots/320/1.jpg",
+subtitle: "Первая линия • ~100 м до моря"
+},
+
+{
+title: "220 соток",
+link: "plot-220.html",
+image: "img/plots/220/1.jpg",
+subtitle: "Первая линия • инвестиционный участок"
+},
+
+{
+title: "140 соток",
+link: "plot-140.html",
+image: "img/plots/140/1.jpg",
+subtitle: "Вторая линия • ~180 м до моря"
+},
+
+{
+title: "6–7 соток",
+link: "plots-6-7.html",
+image: "img/plots/6-7/3.jpg",
+subtitle: "Массив частных участков"
+},
+
+{
+title: "9 соток",
+link: "plot-9.html",
+image: "img/plots/9/2.jpg",
+subtitle: "ИЖС • участок под жилой дом"
+}
+
+]
+
+};
